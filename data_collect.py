39a5dc470e5cd14543c291eed1572edbe743e5b4
Paweł Lidwin <62100117+imLinguin@users.noreply.github.com>
2021-05-01 21:54:46 +0200

    Typo

--- data_collect.py
+++ data_collect.py
@@ -2,7 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="SPOTIFY_CLIENT_ID",
-                                                           client_secret="SPOTIFY_CLIENT_ID"))
+                                                           client_secret="SPOTIFY_CLIENT_SECRET"))
 
 
 def get_spotify_title_data(args):
