0fdcafd82cc1985c1ee461ba4a628bcee148cbf6
Paweł Lidwin <lidwinpawel@gmail.com>
2021-05-02 19:04:26 +0200

    Revert "Better logging and query classification"
    This reverts commit 975d00aa805339f4d075fffb80458c9c6f931bfe.

--- main.py
+++ main.py
@@ -2,61 +2,39 @@ import argparse
 import data_collect
 import youtube
 import os
-import sys
 import re
 
-illegal_chars = ["/", "\\", "|", "\'", "*", "?", "<", ">", ":"]
-chars_to_escape = ["$", "&", "#"]
+illegalChars = ["/", "\\", "|", "\'", "*", "?", "<", ">", ":"]
+
 
 # Ensures that there are no illegal characters in song name
 def make_save_name(name):
     output = name
-    for char in illegal_chars:
+    for char in illegalChars:
         output = output.replace(char, "_")
     return output
 
-# Escapes sh specific characters
-def escape_chars(name):
-    for char in chars_to_escape:
-        name = name.replace(char, "\{0}".format(char))
-    return name
-
-
-def classify_query(query):
-    if not query:
-        return None
-    
-    if re.search("spotify.com[\/:]playlist[\/:](.+)[\s?]",query):
-        return "playlist"
-    elif re.search("spotify.com[\/:]album[\/:](.+)[\s?]",query):
-        return "album"
-    else:
-        return "custom"
-
 
 def attach_metadata(filename, artist, album, cover, name):
-    sys.stdout.write("\rAdding metadata...")
-    os.system('ffmpeg -i "Tmp/{0}.mp3" -i {3} -map 0 -map 1:0 -y -loglevel panic -hide_banner -codec copy '
-              '-metadata "artist={1}" -metadata "album={2}" Out/"{4}.mp3"'.format(escape_chars(filename),
-                                                                                  artist,
+    os.system('ffmpeg -i Tmp/"{0}.mp3" -i {3} -map 0 -map 1:0 -y -hide_banner -codec copy '
+              '-metadata "artist={1}" -metadata "album={2}" Out/"{4}.mp3"'.format(filename, artist,
                                                                                   album,
                                                                                   cover,
                                                                                   make_save_name(name)))
-    print("✅")
     os.remove("./Tmp/{0}.mp3".format(filename))
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download desired song with metadata. Please put values in quotes')
-    parser.add_argument("query", type=str, help="Title or url of the song, url of playlist, url of album")
+    parser = argparse.ArgumentParser(description='Download desired song with metadata. Please put names in quotes')
+    parser.add_argument("--title", type=str, help="Title of the song we are looking for")
+    parser.add_argument("--album", type=str, help="Title of the album")
+    parser.add_argument("--playlist", type=str, help="Spotify playlist url")
 
     args = parser.parse_args()
 
-    classified = classify_query(args.query)
-    
     # Execute if title is provided
-    if classified == "custom":
-        results = data_collect.get_spotify_title_data(args.query)
+    if args.title:
+        results = data_collect.get_spotify_title_data(args)
         data = results["tracks"]["items"][0]
 
         name = data["name"]
@@ -65,12 +43,13 @@ def main():
         cover = data["album"]["images"][0]["url"]
 
         print("{0} - {1}".format(name, artists[0]["name"]))
+        print(cover)
         filename = youtube.download(("{1} {0}".format(name, artists[0]["name"])))
         attach_metadata(filename, artists[0]["name"], album, cover, name)
 
     # Else do same thing but with album
-    elif classified == "album":
-        results = data_collect.get_spotify_album_data(args.query)
+    elif args.album:
+        results = data_collect.get_spotify_album_data(args)
         cover = results[0]
         data = results[1]
         album = results[2]
@@ -78,27 +57,33 @@ def main():
         for track in data["items"]:
             name = track['name']
             artists = track["artists"]
-            print("\r{1} {0}".format(name, artists[0]["name"]))
+            print("{1} {0}".format(name, artists[0]["name"]))
+            print(cover)
             filename = youtube.download(("{1} {0}".format(name, artists[0]["name"])))
 
             attach_metadata(filename, artists[0]["name"], album, cover, name)
 
-    elif classified == "playlist":        
-        # Extract playlist ID from URL
-        question_mark_pos = args.query.index("?")
-        playlist_id = args.query[34:question_mark_pos]
-        tracks = data_collect.get_spotify_playlist_tracks(playlist_id)
+    elif args.playlist:
+        if not re.match("https://(open.spotify.com)/playlist/", args.playlist):
+            print("Invalid playlist URL provided")
+            return
+        else:
+            # Extract playlist ID from URL
+            question_mark_pos = args.playlist.index("?")
+            playlist_id = args.playlist[34:question_mark_pos]
+            tracks = data_collect.get_spotify_playlist_tracks(playlist_id)
 
-        for track in tracks["items"][:100]:
-            name = track["track"]["name"]
-            artists = track["track"]["artists"]
-            album = track["track"]["album"]["name"]
-            cover = track["track"]["album"]["images"][0]["url"]
-            print("{0} - {1}".format(name, artists[0]["name"]))
-            filename = youtube.download(("{1} {0}".format(name, artists[0]["name"])))
+            for track in tracks["items"][:100]:
+                name = track["track"]["name"]
+                artists = track["track"]["artists"]
+                album = track["track"]["album"]["name"]
+                cover = track["track"]["album"]["images"][0]["url"]
+                print("{0} - {1}".format(name, artists[0]["name"]))
+                print(cover)
+                filename = youtube.download(("{1} {0}".format(name, artists[0]["name"])))
 
-            attach_metadata(filename, artists[0]["name"], album, cover, name)
-    print("Done")
+                attach_metadata(filename, artists[0]["name"], album, cover, name)
+
 
 if __name__ == '__main__':
     main()
--- youtube.py
+++ youtube.py
@@ -1,5 +1,4 @@
 import youtube_dl
-import sys
 
 ytdl_opts = {
     "format": "bestaudio/best",
@@ -7,7 +6,6 @@ ytdl_opts = {
     "outtmpl": "Tmp/%(title)s.%(ext)s",
     "noprogress": True,
     "noplaylist": True,
-    "quiet":True,
     "postprocessors": [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -19,11 +17,11 @@ ytdl = youtube_dl.YoutubeDL(ytdl_opts)
 
 
 def download(query):
-    sys.stdout.write("\rDownloading...")
     downloaded = ytdl.extract_info(query, download=True)
     if downloaded["entries"]:
         downloaded = downloaded['entries'][0]
     filename = ytdl.prepare_filename(downloaded)
     dot = filename.rindex(".")
     name = filename[4:dot]
+    print(name)
     return name
