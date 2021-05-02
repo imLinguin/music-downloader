975d00aa805339f4d075fffb80458c9c6f931bfe
Paweł Lidwin <lidwinpawel@gmail.com>
2021-05-02 18:57:33 +0200

    Better logging and query classification

--- data_collect.py
+++ data_collect.py
@@ -1,20 +1,23 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="SPOTIFY_CLIENT_ID",
-                                                           client_secret="SPOTIFY_CLIENT_SECRET"))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="caf7251809ae492a93012c0ccd5fd0d0",
+                                                           client_secret="f9032fdb6ddb44a7839ea481492de609"))
 
 
-def get_spotify_title_data(args):
-    results = sp.search(q=args.title, limit=1, type="track")
+def get_spotify_title_data(query):
+    results = sp.search(q=query, limit=1, type="track")
     return results
 
 
 def get_spotify_album_data(args):
-    results = sp.search(q=args.album, limit=1, type="album")
-    cover = results["albums"]["items"][0]["images"][0]["url"]
-    album = results["albums"]["items"][0]["name"]
-    tracks = sp.album_tracks(results["albums"]["items"][0]["id"])
+    question_mark_pos = args.index("?")
+    album_id = args[31:question_mark_pos]
+
+    album_data = sp.album(album_id)
+    tracks = sp.album_tracks(album_id, limit=50)
+    cover = album_data["images"][0]["url"]
+    album = album_data["name"]
     return [cover, tracks, album]
 
 
--- main.py
+++ main.py
@@ -2,39 +2,61 @@ import argparse
 import data_collect
 import youtube
 import os
+import sys
 import re
 
-illegalChars = ["/", "\\", "|", "\'", "*", "?", "<", ">", ":"]
-
+illegal_chars = ["/", "\\", "|", "\'", "*", "?", "<", ">", ":"]
+chars_to_escape = ["$", "&", "#"]
 
 # Ensures that there are no illegal characters in song name
 def make_save_name(name):
     output = name
-    for char in illegalChars:
+    for char in illegal_chars:
         output = output.replace(char, "_")
     return output
 
+# Escapes sh specific characters
+def escape_chars(name):
+    for char in chars_to_escape:
+        name = name.replace(char, "\{0}".format(char))
+    return name
+
+
+def classify_query(query):
+    if not query:
+        return None
+    
+    if re.search("spotify.com[\/:]playlist[\/:](.+)[\s?]",query):
+        return "playlist"
+    elif re.search("spotify.com[\/:]album[\/:](.+)[\s?]",query):
+        return "album"
+    else:
+        return "custom"
+
 
 def attach_metadata(filename, artist, album, cover, name):
-    os.system('ffmpeg -i Tmp/"{0}.mp3" -i {3} -map 0 -map 1:0 -y -hide_banner -codec copy '
-              '-metadata "artist={1}" -metadata "album={2}" Out/"{4}.mp3"'.format(filename, artist,
+    sys.stdout.write("\rAdding metadata...")
+    os.system('ffmpeg -i "Tmp/{0}.mp3" -i {3} -map 0 -map 1:0 -y -loglevel panic -hide_banner -codec copy '
+              '-metadata "artist={1}" -metadata "album={2}" Out/"{4}.mp3"'.format(escape_chars(filename),
+                                                                                  artist,
                                                                                   album,
                                                                                   cover,
                                                                                   make_save_name(name)))
+    print("✅")
     os.remove("./Tmp/{0}.mp3".format(filename))
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download desired song with metadata. Please put names in quotes')
-    parser.add_argument("--title", type=str, help="Title of the song we are looking for")
-    parser.add_argument("--album", type=str, help="Title of the album")
-    parser.add_argument("--playlist", type=str, help="Spotify playlist url")
+    parser = argparse.ArgumentParser(description='Download desired song with metadata. Please put values in quotes')
+    parser.add_argument("query", type=str, help="Title or url of the song, url of playlist, url of album")
 
     args = parser.parse_args()
 
+    classified = classify_query(args.query)
+    
     # Execute if title is provided
-    if args.title:
-        results = data_collect.get_spotify_title_data(args)
+    if classified == "custom":
+        results = data_collect.get_spotify_title_data(args.query)
         data = results["tracks"]["items"][0]
 
         name = data["name"]
@@ -43,13 +65,12 @@ def main():
         cover = data["album"]["images"][0]["url"]
 
         print("{0} - {1}".format(name, artists[0]["name"]))
-        print(cover)
         filename = youtube.download(("{1} {0}".format(name, artists[0]["name"])))
         attach_metadata(filename, artists[0]["name"], album, cover, name)
 
     # Else do same thing but with album
-    elif args.album:
-        results = data_collect.get_spotify_album_data(args)
+    elif classified == "album":
+        results = data_collect.get_spotify_album_data(args.query)
         cover = results[0]
         data = results[1]
         album = results[2]
@@ -57,33 +78,27 @@ def main():
         for track in data["items"]:
             name = track['name']
             artists = track["artists"]
-            print("{1} {0}".format(name, artists[0]["name"]))
-            print(cover)
+            print("\r{1} {0}".format(name, artists[0]["name"]))
             filename = youtube.download(("{1} {0}".format(name, artists[0]["name"])))
 
             attach_metadata(filename, artists[0]["name"], album, cover, name)
 
-    elif args.playlist:
-        if not re.match("https://(open.spotify.com)/playlist/", args.playlist):
-            print("Invalid playlist URL provided")
-            return
-        else:
-            # Extract playlist ID from URL
-            question_mark_pos = args.playlist.index("?")
-            playlist_id = args.playlist[34:question_mark_pos]
-            tracks = data_collect.get_spotify_playlist_tracks(playlist_id)
+    elif classified == "playlist":        
+        # Extract playlist ID from URL
+        question_mark_pos = args.query.index("?")
+        playlist_id = args.query[34:question_mark_pos]
+        tracks = data_collect.get_spotify_playlist_tracks(playlist_id)
 
-            for track in tracks["items"][:100]:
-                name = track["track"]["name"]
-                artists = track["track"]["artists"]
-                album = track["track"]["album"]["name"]
-                cover = track["track"]["album"]["images"][0]["url"]
-                print("{0} - {1}".format(name, artists[0]["name"]))
-                print(cover)
-                filename = youtube.download(("{1} {0}".format(name, artists[0]["name"])))
+        for track in tracks["items"][:100]:
+            name = track["track"]["name"]
+            artists = track["track"]["artists"]
+            album = track["track"]["album"]["name"]
+            cover = track["track"]["album"]["images"][0]["url"]
+            print("{0} - {1}".format(name, artists[0]["name"]))
+            filename = youtube.download(("{1} {0}".format(name, artists[0]["name"])))
 
-                attach_metadata(filename, artists[0]["name"], album, cover, name)
-
+            attach_metadata(filename, artists[0]["name"], album, cover, name)
+    print("Done")
 
 if __name__ == '__main__':
     main()
